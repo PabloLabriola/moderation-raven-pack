b24f44dfb6db8fa2a6ef224229cc8d9eba52c0fd
Pablo <pllabriola@gmail.com>
2024-05-07 16:52:47 +0200

    Moderation App Raven Pack - First Commit

---
+++ src/main/java/com/ravenpacktest/moderationms/ModerationMsApplication.java
@@ -0,0 +1,13 @@
+package com.ravenpacktest.moderationms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModerationMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModerationMsApplication.class, args);
+	}
+
+}
